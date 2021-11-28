42eaaf6f069da2d3593b8fc56fc236b548c055e8
playerplayer <bohdan.k2000@gmail.com>
2021-11-27 20:45:08 -0500

    Minor improvements

--- app/src/main/java/edu/temple/audiobb/ControlFragment.kt
+++ app/src/main/java/edu/temple/audiobb/ControlFragment.kt
@@ -1,16 +1,26 @@
 package edu.temple.audiobb
 
+import android.media.Image
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.ImageButton
+import android.widget.SeekBar
+import android.widget.TextView
 private const val ARG_BOOK = "book"
 
 class ControlFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private lateinit var param_book: Book
+    //buttons
+    private lateinit var btnPlay: ImageButton
+    private lateinit var btnPause: ImageButton
+    private lateinit var btnStop: ImageButton
+    private lateinit var seekBar: SeekBar
+    private lateinit var txtTime: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +34,15 @@ class ControlFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_control, container, false)
+        val v: View = inflater.inflate(R.layout.fragment_control, container, false)
+
+        btnPlay = v.findViewById(R.id.btnPlay)
+        btnPause = v.findViewById(R.id.btnPause)
+        btnStop = v.findViewById(R.id.btnStop)
+        seekBar = v.findViewById(R.id.seekBar)
+        txtTime = v.findViewById(R.id.txtTime)
+
+        return v
     }
 
     companion object {
